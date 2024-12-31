b5d2bb1161737cb1d49d5860c98457d7bbc26436
Cisco <dcamp090799@gmail.com>
2024-12-30 19:53:19 -0500

    add scripts folder with index.js file to add initial cards. add script to bottom of html above the closing body.

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "Time lapse of stars in PR",
+    link: "https://unsplash.com/photos/time-lapse-photography-of-star-273_wcQPfYw",
+  },
+  {
+    name: "Drip coffee! Yum!",
+    link: "https://unsplash.com/photos/person-pouring-coffee-on-white-ceramic-cup-U4euPE63P3E",
+  },
+  {
+    name: "Bowl of ramen",
+    link: "https://unsplash.com/photos/sliced-bread-on-black-ceramic-plate-CnY6_UFphs0",
+  },
+  {
+    name: "Doing yoga at sunset",
+    link: "https://unsplash.com/photos/silhouette-of-person-performing-yoga-4InW0sYpW-M",
+  },
+  {
+    name: "Going back to them old school games",
+    link: "https://unsplash.com/photos/a-person-holding-a-gameboy-in-their-hand-gsbpSj0buQg",
+  },
+  {
+    name: "One of the Great Pyramids of Giza",
+    link: "https://unsplash.com/photos/a-pyramid-in-the-desert-GsZsn8SaUzg",
+  },
+];
+
+console.log(initialCards);
