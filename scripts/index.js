a51dc6bbab72678ef572d8983f2b62a8af950411
Cisco <dcamp090799@gmail.com>
2025-01-04 22:29:30 -0500

    modify modal.css, index.html, index.js. add queries and functions to modal to open and close.

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,19 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditButton = document.querySelector(".profile__edit-btn");
+
+const editModal = document.querySelector("#edit-modal");
+const editModalClosebtn = editModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+editModalClosebtn.addEventListener("click", closeModal);
