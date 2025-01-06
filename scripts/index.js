1ec859730006c1ace70b60078891f2d9cf89cd65
Damian <dcamp090799@gmail.com>
2025-01-05 21:54:03 -0500

    Modify scripts/index.js to include name and description functionality when save is clicked. Rename label ids in html.

--- scripts/index.js
+++ scripts/index.js
@@ -26,18 +26,32 @@ const initialCards = [
 ];
 
 const profileEditButton = document.querySelector(".profile__edit-btn");
+const profileName = document.querySelector(".profile__name");
+const profileDesc = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#edit-modal");
+const editFormElement = editModal.querySelector(".modal__form");
 const editModalClosebtn = editModal.querySelector(".modal__close-btn");
+const editModalNameInput = editModal.querySelector("#profile-name-input");
+const editModalDescInput = editModal.querySelector("#profile-desc-input");
 
 function openModal() {
+  editModalNameInput.value = profileName.textContent;
+  editModalDescInput.value = profileDesc.textContent;
   editModal.classList.add("modal_opened");
 }
-
-profileEditButton.addEventListener("click", openModal);
 
 function closeModal() {
   editModal.classList.remove("modal_opened");
 }
 
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = editModalNameInput.value;
+  profileDesc.textContent = editModalDescInput.value;
+  closeModal();
+}
+
+profileEditButton.addEventListener("click", openModal);
 editModalClosebtn.addEventListener("click", closeModal);
+editFormElement.addEventListener("submit", handleEditFormSubmit);
