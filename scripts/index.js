eee428ae3af710a7e96e5ad91a16dd0d3bbfb830
Damian <dcamp090799@gmail.com>
2025-01-06 00:05:49 -0500

    remove console.log from line 46

--- scripts/index.js
+++ scripts/index.js
@@ -44,8 +44,6 @@ const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
 
 function getCardElement(data) {
-  console.log(data);
-
   const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
   const cardNameEl = cardElement.querySelector(".card__title");
   const cardImageEl = cardElement.querySelector(".card__image");
