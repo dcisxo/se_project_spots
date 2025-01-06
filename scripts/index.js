df597ee0ad24ae865ed7d6467c4e93c771abb433
Damian <dcamp090799@gmail.com>
2025-01-05 23:30:23 -0500

    Modify scripts/index.js to include the description and image of updated pictures. Add template to html to upload pictures.

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,33 @@
 const initialCards = [
   {
     name: "Time lapse of stars in PR",
-    link: "https://unsplash.com/photos/time-lapse-photography-of-star-273_wcQPfYw",
+    link: "https://images.unsplash.com/photo-1542314490-be9a382dbbb2?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Time lapse of the stars in Puerto Rico"
   },
   {
     name: "Drip coffee! Yum!",
-    link: "https://unsplash.com/photos/person-pouring-coffee-on-white-ceramic-cup-U4euPE63P3E",
+    link: "https://images.unsplash.com/photo-1617590591232-26315f8482cb?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Guy pouring water into filter for drip coffee"
   },
   {
     name: "Bowl of ramen",
-    link: "https://unsplash.com/photos/sliced-bread-on-black-ceramic-plate-CnY6_UFphs0",
+    link: "https://images.unsplash.com/photo-1603911036164-f2ef8a24e235?q=80&w=1364&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Beautiful bowl of ramen with pork belly and poached egg"
   },
   {
     name: "Doing yoga at sunset",
-    link: "https://unsplash.com/photos/silhouette-of-person-performing-yoga-4InW0sYpW-M",
+    link: "https://images.unsplash.com/photo-1516827003699-2880f453d93b?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Silhouette of person doing yoga at sunset"
   },
   {
     name: "Going back to them old school games",
-    link: "https://unsplash.com/photos/a-person-holding-a-gameboy-in-their-hand-gsbpSj0buQg",
+    link: "https://images.unsplash.com/photo-1708958624305-8d4c083bdba6?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "Holding an original transparent Gameboy with Super Mario Land 2 on the screen"
   },
   {
     name: "One of the Great Pyramids of Giza",
-    link: "https://unsplash.com/photos/a-pyramid-in-the-desert-GsZsn8SaUzg",
+    link: "https://images.unsplash.com/photo-1652150055994-2ca33da6fed6?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    alt: "One of the Great Pyramids of Giza"
   },
 ];
 
@@ -34,6 +40,24 @@ const editFormElement = editModal.querySelector(".modal__form");
 const editModalClosebtn = editModal.querySelector(".modal__close-btn");
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescInput = editModal.querySelector("#profile-desc-input");
+const cardTemplate = document.querySelector("#card-template");
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(data) {
+  console.log(data);
+
+  const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+
+  cardNameEl.textContent = data.name;
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.alt;
+
+
+
+  return cardElement;
+}
 
 function openModal() {
   editModalNameInput.value = profileName.textContent;
@@ -55,3 +79,8 @@ function handleEditFormSubmit(evt) {
 profileEditButton.addEventListener("click", openModal);
 editModalClosebtn.addEventListener("click", closeModal);
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+for (let i = 0; i < initialCards.length; i++) {
+  const cardElement = getCardElement(initialCards[i]);
+  cardsList.prepend(cardElement);
+}
